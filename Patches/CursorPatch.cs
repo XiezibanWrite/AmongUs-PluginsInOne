7f1c41063a71728072a0789c96d4299953838d79
XiezibanWrite <103623519+XiezibanWrite@users.noreply.github.com>
2023-08-19 11:40:38 +0800

    Update CursorPatch.cs

--- Patches/CursorPatch.cs
+++ Patches/CursorPatch.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using System.IO;
 using System.Reflection;
 using System.Collections.Generic;
+using PluginsInOne.Modules;
 
 namespace PluginsInOne.Patches
 {
@@ -21,34 +22,10 @@ namespace PluginsInOne.Patches
         }
         public static void CursorInit()
         {
-            Sprite sprite = LoadSprite("PluginsInOne.Resources.Cursor.png");
+            //才想起来我有PictureLoad这个可以用 笑死 
+            Sprite sprite = PictureLoad.LoadSprite("PluginsInOne.Resources.Cursor.png");
             Cursor.SetCursor(sprite.texture, Vector2.zero, CursorMode.Auto);
         }
-        public static Sprite? LoadSprite(string path, float pixelsPerUnit = 1f)
-        {
-            try
-            {
-                if (CachedSprites.TryGetValue(path + pixelsPerUnit, out var sprite)) return sprite;
-                Texture2D texture = LoadTextureFromResources(path);
-                sprite = Sprite.Create(texture, new(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), pixelsPerUnit);
-                sprite.hideFlags |= HideFlags.HideAndDontSave | HideFlags.DontSaveInEditor;
-                return CachedSprites[path + pixelsPerUnit] = sprite;
-            }
-            catch
-            {
-                // ignored
-            }
 
-            return null;
-        }
-        public static Texture2D LoadTextureFromResources(string path)
-        {
-            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
-            var texture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
-            using MemoryStream ms = new();
-            stream.CopyTo(ms);
-            ImageConversion.LoadImage(texture, ms.ToArray(), false);
-            return texture;
-        }
     }
 }
